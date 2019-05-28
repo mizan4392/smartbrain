2dda47f60af10cd4faacd2bc7fbdc6cdd2429062
mizan4392 <md.mizan4392@gmail.com>
2019-05-28 10:10:51 +0600

    Added 1 new components 'rank & added some particals effect '

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const params={
+  particles: {
+    number:{
+      value:40,
+      density:{
+        enable:true,
+        value_area:200
+      }
+    }
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      <Particles params={params} className='particales'/>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
