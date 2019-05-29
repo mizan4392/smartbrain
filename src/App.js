6ad9e4d0f2d2ddb76bd4882b0f8ebf558b98bdcf
mizan4392 <md.mizan4392@gmail.com>
2019-05-29 09:30:20 +0600

    Added Clarifai FACE_DETECT_MODEL to the app

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const params={
   }
 }
 
-
+ 
 
 class App extends React.Component {
 
@@ -35,20 +35,27 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      input:''
+      input:'',
+      imageUrl:''
     }
   }
 
   onInputChange =(event) =>{
-    console.log(event.target.value);
+    //console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
 
   onButtonSubmit = () =>{
-    console.log('clicked');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+
+    this.setState({imageUrl:this.state.input});
+    app.models
+      .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
       function(response) {
         // do something with response
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function(err) {
         // there was an error
@@ -70,7 +77,7 @@ class App extends React.Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 
-const FaceRecognition = () =>{
+const FaceRecognition = ({imageUrl}) =>{
 
     return(
-        <div className='center'>
-            <img src={"https://samples.clarifai.com/face-det.jpg"} alt='image'></img>
+        <div>
+            <div className='center ma'>
+                <div className='absolute mt2'>
+                  <img src={imageUrl} width='500px' height='auto'></img>
+                </div>
+            </div>
+            <div style={{height:'200px'}}>
+
+            </div>
         </div>
     );
 }
